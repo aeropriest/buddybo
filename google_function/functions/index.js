8ba6cb2e9dacfcf292507a98d6464bc87a2f25de
aeropriest <ashok.jaiswal@gmail.com>
2025-02-08 11:59:14 +0800

    * Added the gemini

--- google_function/functions/index.js
+++ google_function/functions/index.js
@@ -6,8 +6,70 @@ const { getFirestore } = require("firebase-admin/firestore");
 const OpenAI = require("openai");
 const { VertexAI } = require("@google-cloud/vertexai");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { Readable } = require("stream");
 
 initializeApp();
+
+const fillers = {
+  Bible: [
+    "I'm so glad you asked that!",
+    "That's a wonderful question about faith!",
+    "It's great to be curious about God's stories!",
+    "What a thoughtful question!",
+    "I'm excited to share this with you!",
+    "That's a beautiful question to explore!",
+    "I love your curiosity about the Bible!",
+    "What an interesting thing to think about!",
+    "I'm happy to help you learn more!",
+    "That's a fantastic question about our faith!",
+  ],
+  Science: [
+    "I'm thrilled you want to know more!",
+    "What a great question about science!",
+    "It's awesome to be curious about the world!",
+    "I'm excited to explore this with you!",
+    "That's a fascinating topic to dive into!",
+    "Your curiosity is wonderful—let's find out!",
+    "That's a smart question to ask!",
+    "I love how eager you are to learn!",
+    "What an interesting thing to discover together!",
+    "I'm here to help you understand better!",
+  ],
+  Language: [
+    "Oh, that's a fun question about words!",
+    "I'm so glad you're curious about language!",
+    "What a delightful question to explore together!",
+    "It's wonderful that you want to learn more!",
+    "I love your enthusiasm for language!",
+    "That's a great thing to think about with words!",
+    "Your curiosity is inspiring—let's rhyme it out!",
+    "What an exciting topic to discuss!",
+    "I'm here to help you play with language!",
+    "That's a clever question—let's explore it!",
+  ],
+  History: [
+    "What an intriguing question about history!",
+    "I'm so glad you're interested in our past!",
+    "It's wonderful to be curious about history!",
+    "What a great topic for us to explore together!",
+    "Your interest in history is fantastic—let's dive in!",
+    "That's a thoughtful question about our world’s story!",
+    "I love how eager you are to learn about the past!",
+    "What an exciting thing to uncover together!",
+    "I'm here to help you learn more about history!",
+    "That’s a brilliant question—let’s discover the answer!",
+  ],
+};
+
+function getRandomFiller(persona) {
+  const personaFillers = fillers[persona];
+  if (!personaFillers) {
+    throw new Error("Invalid persona specified.");
+  }
+
+  const randomIndex = Math.floor(Math.random() * personaFillers.length);
+  return personaFillers[randomIndex];
+}
 
 exports.askGemini = onRequest(async (req, res) => {
   const question = req.query.question;
@@ -16,6 +78,72 @@ exports.askGemini = onRequest(async (req, res) => {
   const answer = result.response.text();
   console.log(answer);
   res.json({ answer });
+});
+
+exports.numbersStream = onRequest((request, response) => {
+  // Set headers for streaming
+  response.setHeader("Content-Type", "text/plain");
+  response.setHeader("Transfer-Encoding", "chunked");
+
+  // Create a readable stream
+  const randomNumberStream = new Readable({
+    read() {
+      for (let i = 0; i < 100; i++) {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        this.push(randomNumber.toString());
+        this.push("\n");
+      }
+      this.push(null);
+    },
+  });
+
+  randomNumberStream.pipe(response);
+});
+
+exports.askOpenAIStream = onRequest(async (req, res) => {
+  const gpt = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+  const question = req.query.question;
+  const messages = [{ role: "user", content: question }];
+
+  console.log("----- streaming request ----------------", question);
+
+  try {
+    const response = await gpt.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages,
+      stream: true,
+    });
+
+    res.setHeader("Content-Type", "text/plain");
+    const stream = new ReadableStream({
+      async pull(controller) {
+        for await (const chunk of response) {
+          if (chunk.choices[0].delta.content) {
+            console.log("got", chunk.choices[0].delta.content);
+            controller.enqueue(chunk.choices[0].delta.content);
+          }
+        }
+        controller.close();
+      },
+    });
+
+    const reader = stream.getReader();
+
+    const push = async () => {
+      const { done, value } = await reader.read();
+      if (done) {
+        return res.end(); // End response when done
+      }
+      console.log("Sending ", value);
+      res.write(value); // Write each chunk to the response
+      push(); // Continue reading
+    };
+
+    push();
+  } catch (error) {
+    console.error("Error fetching data from OpenAI:", error);
+    res.status(500).json({ error: "Error fetching data from OpenAI" });
+  }
 });
 
 exports.askOpenAI = onRequest(async (req, res) => {
@@ -48,6 +176,7 @@ const getModel = (modelName) => {
 };
 
 exports.askQuestion = onRequest(async (req, res) => {
+  console.log(req.query);
   const { model, question } = req.query;
   const selectedModel = model || "gemini";
   const modelInstance = getModel(selectedModel);
@@ -64,8 +193,43 @@ exports.askQuestion = onRequest(async (req, res) => {
         model: "gpt-3.5-turbo",
         temperature: 0.1,
         messages: messages,
+        // stream: true,
       });
       answer = completion.choices[0].message.content;
+      // //-------- streaming start --------------------
+      // const response = await modelInstance.chat.completions.create({
+      //   model: "gpt-3.5-turbo",
+      //   temperature: 0.1,
+      //   messages: messages,
+      //   stream: true,
+      // });
+
+      // const stream = new ReadableStream({
+      //   async pull(controller) {
+      //     for await (const chunk of response) {
+      //       if (chunk.choices[0].delta.content) {
+      //         controller.enqueue(chunk.choices[0].delta.content);
+      //       }
+      //     }
+      //     controller.close();
+      //   },
+      // });
+
+      // // Set headers for streaming
+      // res.setHeader("Content-Type", "text/plain");
+
+      // // Stream response back to client
+      // stream
+      //   .getReader()
+      //   .read()
+      //   .then(function processText({ done, value }) {
+      //     if (done) {
+      //       return res.end(); // End response when done
+      //     }
+      //     res.write(value); // Write each chunk to the response
+      //     return stream.getReader().read().then(processText); // Continue reading
+      //   });
+      // //-------- streaming end --------------------
     } else {
       answer =
         "Please select one of the following models: 1. Gemini, 2. OpenAi";
@@ -116,7 +280,7 @@ const getAnswer = async (macid, question, role) => {
   console.time("getAnswerExecutionTime"); // Start timer
   const startTime = performance.now(); // Start high-resolution timer
 
-  let answer;
+  let answer, segue;
   try {
     messages.push({ role: "user", content: question });
     const model = getModel(useModel);
@@ -125,18 +289,39 @@ const getAnswer = async (macid, question, role) => {
       const result = await model.generateContent(question);
       answer = result.response.text();
     } else if (useModel === "openai") {
-      const completion = await model.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        temperature: 0.1,
-        messages: messages,
-      });
-      answer = completion.choices[0].message.content;
+      {
+        const completion = await model.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          temperature: 0.1,
+          messages: messages,
+        });
+        answer = completion.choices[0].message.content;
+        console.log("--------------------------------");
+        console.log(messages);
+        console.log("--------------------------------");
+      }
+      // {
+      //   const completion = await model.chat.completions.create({
+      //     model: "gpt-3.5-turbo",
+      //     temperature: 0.1,
+      //     messages: [
+      //       {
+      //         role: "user",
+      //         // content: `Provide a concise segue summary sentence of 4-10 words for the following answer that would be prepended to the next question \n ${answer}`,
+      //         content: `Based on the previous answer, summarize it in 4-10 words that connects to the current question:\nPrevious Answer: ${answer}`,
+      //       },
+      //     ],
+      //   });
+      //   segue = completion.choices[0].message.content;
+      // }
     }
 
     console.log("-------------------- prompt --------------------------------");
     console.log(messages);
     console.log("-------------------- answer --------------------------------");
     console.log(answer);
+    console.log("-------------------- segue --------------------------------");
+    console.log(JSON.stringify(segue));
   } catch (error) {
     logger.error("Getting answer from model :", error);
     throw new Error("Failed to get the answer from " + useModel);
@@ -156,19 +341,58 @@ const getAnswer = async (macid, question, role) => {
 };
 
 exports.askBuddy = onRequest(async (req, res) => {
-  const startTime = performance.now(); // Start high-resolution timer
   const macid = req.query.macid;
-
+  const persona = req.query.persona;
   const question = req.query.question;
-  const role = `You are a teacher for 3-8 years old. Answer the children with 
-  facts in 2-6 lines. If there is no answer found, encourage them to ask 
-  something else with a leading question related to this conversation.`;
-  const answer = await getAnswer(macid, question, role);
-  res.json(answer);
-  const endTime = performance.now(); // End high-resolution timer
-  const elapsedTime = endTime - startTime; // Calculate elapsed time
-  console.log("------ time taken -------");
-  console.log(elapsedTime);
+  const filler = getRandomFiller(persona);
+
+  logger.info("askBuddy called", req.query);
+
+  switch (persona) {
+    case "Bible":
+      role = `You are a friendly and gentle teacher named Mary who loves to 
+        share stories and lessons from the Bible with children aged 3 to 8 years old.
+        When answering their questions, use simple words and concepts they can
+        understand and keep it short 4-6 lines. Make your explanations fun and 
+        engaging, often using stories, parables, and examples from the Bible. 
+        Always be kind, patient, and encouraging, helping the children to learn 
+        about Jesus, His love, and the Christian faith. After providing your 
+        answer, ask a leading question to inspire the child to ask more about 
+        the teachings of the Bible.`;
+      break;
+    case "Science":
+      role = `You are a friendly and enthusiastic science teacher named
+        Mary who loves to explain scientific concepts to children aged 3 to 8 
+        years old. When answering their questions, use simple words and concepts 
+        they can understand. Make your explanations fun and engaging, often using
+        interesting facts, stories, and examples. Always be kind, patient, and 
+        encouraging, helping the children to develop a love for science and 
+        curiosity about the world around them. After providing your answer, ask
+        only ONE leading question to inspire the child to ask more about science.`;
+      break;
+    case "Language":
+      role = `You are a friendly and enthusiastic language teacher named 
+        Mary who loves to explain language concepts to children aged 3 to 8 years 
+        old. When answering their questions, use simple words and make your 
+        answers rhyme. Make your explanations fun and engaging, often using 
+        interesting facts, stories, and examples. Always be kind, patient, and 
+        encouraging, helping the children to develop a love for language and 
+        curiosity about words. After providing your answer, ask only ONE leading 
+        question to inspire the child to ask more about language.`;
+      break;
+    case "History":
+      role = `You are a knowledgeable history teacher who loves sharing fascinating 
+        stories about our past with children aged 3 to 8 years old. Use simple words 
+        and concepts they can understand while making your explanations fun and engaging.
+        Encourage them with interesting facts about historical events or figures. After 
+        providing your answer, ask a leading question that inspires them to learn more about history.`;
+      break;
+    default:
+      return res.status(400).json({ error: "Invalid persona specified." });
+  }
+
+  const { answer } = await getAnswer(macid, question, role);
+  res.json({ answer, filler });
 });
 
 exports.askBible = onRequest(async (req, res) => {
@@ -184,7 +408,8 @@ exports.askBible = onRequest(async (req, res) => {
     answer, ask a leading question to inspire the child to ask more about 
     the teachings of the Bible. Keep the answers 4-6 lines long, be more 
     subtle mentioning Bible, Jesus, and God.`;
-  const answer = await getAnswer(macid, question, role);
+  // const answer = await getAnswer(macid, question, role);
+  const answer = "God made it all, he takes care of all the children";
   res.json(answer);
 });
 
